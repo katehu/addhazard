b3ea27d1107937b6dafd6827f9670104adb1fa72
Jie Hu <Kate@Jies-MacBook-Air.local>
2016-11-07 23:40:23 -0800

    use nwts2ph in the example; clean typo to pass check

--- R/nwtsco.R
+++ R/nwtsco.R
@@ -1,4 +1,4 @@
-#' Dataset from the National Wilms Tumor Study (NWTS) 
+#' Dataset from the National Wilms Tumor Study (NWTS)
 #' @format A data frame with 3915 rows and 12 variables:
 #' \describe{
 #' \item{trel}{Time to relapse orlast date seen (yr), continuous}
@@ -6,18 +6,18 @@
 #' \item{relaps}{Indicator of relapse,
 #'                0 = Alive no prior relapse when last seen,
 #'                1 = Relapsed after trel years}
-#' \item{dead}{Indicator of death, 
+#' \item{dead}{Indicator of death,
 #'               0 = Alive when last seen,
 #'               1 = Died after tsur years}
-#' \item{study}{NWTS study, 
+#' \item{study}{NWTS study,
 #'              3 = NWTS-3,
 #'              4 = NWTS-4}
-#'\item{stage}{Stage of disease, 
+#'\item{stage}{Stage of disease,
 #'             1=I,
 #'             2=II,
 #'             3=III,
 #'             4=IV}
-#' \item{histol}{Central Path histology, 
+#' \item{histol}{Central Path histology,
 #'              0 = Favorable (FH),
 #'              1 = Unfavorable (UH)}
 #' \item{instit}{Institutional histology,
@@ -30,10 +30,9 @@
 #' }
 #' @source \url{http://faculty.washington.edu/norm/datasets/nwts-share.txt}
 #'  Originally used by M. Kulich and D.Y. Lin:
-#'  Improving the efficiency of relative-risk estimation in case-cohort studies. J Amer Statis 
-#'  Assoc 99:832-844, 2004.
+#'  Improving the efficiency of relative-risk estimation in case-cohort studies.
+#'  J Amer Statis Assoc 99:832-844, 2004.
 #' @docType data
 #' @keywords datasets
 #' @name nwtsco
 "nwtsco"
- 
