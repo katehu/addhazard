31334cc95060da77f492543c3ff75482f499d2c7
Jie Hu <Kate@Jies-MacBook-Air.local>
2017-03-24 23:26:40 -0700

    add summary for ah.ph2.only and solve the problem of no rownames for coefficients when there is one covariate

--- R/summary.R
+++ R/summary.R
@@ -9,7 +9,9 @@ summary.ah <- function(object, ...) {
     p.value <- 2 * pnorm(-abs(z))
     TAB <- cbind(coef = object$coef, se = object$se, lower.95 = ci.lower,
         upper.95 = ci.upper, z = z, p.value = p.value)
-    rownames(TAB) = colnames(object$model)[-1]
+    if(dim(TAB)[1] == 1){
+      rownames(TAB) = as.character(object$formula[[3]]) # retrieve the covariate, single continuous covariate
+    }
     res <- list(call = object$call, coefficients = TAB)
     class(res) <- "summary.ah"
     res
@@ -26,7 +28,7 @@ print.summary.ah <- function(x, digits = max(getOption("digits") -
 }
 
 #' @export
-summary.ah.2ph <- function(object, ...) {
+summary.ah.ph2.only <- function(object, ...) {
     options(digits = 5)
     ci.lower <- object$coef - 1.96 * object$se
     ci.upper <- object$coef + 1.96 * object$se
@@ -34,20 +36,53 @@ summary.ah.2ph <- function(object, ...) {
     p.value <- 2 * pnorm(-abs(z))
     TAB <- cbind(coef = object$coef, se = object$se, lower.95 = ci.lower,
         upper.95 = ci.upper, z = z, p.value = p.value)
-    rownames(TAB) = colnames(object$model)[-1]
+    if(dim(TAB)[1] == 1){
+    rownames(TAB) = as.character(object$fit.pha1$formula[[3]]) # retrieve the covariate, single continuous covariate
+    }
     res <- list(call = object$call, coefficients = TAB)
-    class(res) <- "summary.ah"
+    class(res) <- "summary.ah.ph2.only"
     res
 }
 
 
 
 #' @export
-print.summary.ah.2ph <- function(x, digits = max(getOption("digits") -
+print.summary.ah.ph2.only <- function(x, digits = max(getOption("digits") -
     4, 4), signif.stars = getOption("show.signif.stars"), ...) {
     cat("Call:\n")
     print(x$call)
     cat("\n")
     printCoefmat(x$coefficients, digits = digits, signif.stars = signif.stars, P.values = TRUE,
         has.Pvalue=TRUE, ...)
+}
+
+
+#' @export
+summary.ah.2ph <- function(object, ...) {
+  options(digits = 5)
+  ci.lower <- object$coef - 1.96 * object$se
+  ci.upper <- object$coef + 1.96 * object$se
+  z <- object$coef/object$se
+  p.value <- 2 * pnorm(-abs(z))
+  TAB <- cbind(coef = object$coef, se = object$se, lower.95 = ci.lower,
+               upper.95 = ci.upper, z = z, p.value = p.value)
+  rownames(TAB) = colnames(object$model)[-1]
+  if(dim(TAB)[1] == 1){
+    rownames(TAB) = as.character(object$fit.pha1$formula[[3]]) # retrieve the covariate, single continuous covariate
+  }
+  res <- list(call = object$call, coefficients = TAB)
+  class(res) <- "summary.ah.2ph"
+  res
+}
+
+
+
+#' @export
+print.summary.ah.2ph <- function(x, digits = max(getOption("digits") -
+                                                   4, 4), signif.stars = getOption("show.signif.stars"), ...) {
+  cat("Call:\n")
+  print(x$call)
+  cat("\n")
+  printCoefmat(x$coefficients, digits = digits, signif.stars = signif.stars, P.values = TRUE,
+               has.Pvalue=TRUE, ...)
 }
